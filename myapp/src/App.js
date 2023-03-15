9501a1263194b2543b58980775241f76d97014f3
Marwan Hawash <marwanmagdi.2001@gmail.com>
2023-03-15 23:24:11 +0200

    Added them to the navbar and router

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import "./App.css";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -11,6 +11,8 @@ function Navbar() {
           </Typography>
           <Button color="inherit" component={Link} to="/">Home</Button>
           <Button color="inherit" component={Link} to="/about">About</Button>
+          <Button color="inherit" component={Link} to="/login">Login</Button>
+          <Button color="inherit" component={Link} to="/register">Register</Button>
         </Toolbar>
       </AppBar>
   )
