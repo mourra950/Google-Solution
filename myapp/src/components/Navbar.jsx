c643f8563bc6465f513921ecbbb62d5bc64af4ba
Omar <mourra24@gmail.com>
2023-03-16 22:04:01 +0200

    Logo design done
    made a new logo using illustrator and photoshop and added it to the navigation bar of the site

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button} from '@mui/material';
-
+import Logo from "./logo.png"
 function Navbar() {
   return (
       <AppBar position='static' color='inherit'>
         <Toolbar>
+        <img src={Logo} alt='website logo' style={{height:'70px'}}/>
+
           <Typography variant='h4' component="div" sx={{ flexGrow: 1}}>
             Global Care
           </Typography>
+          
           <Button color="inherit" component={Link} to="/">Home</Button>
           <Button color="inherit" component={Link} to="/about">About</Button>
           <Button color="inherit" component={Link} to="/login">Login</Button>
